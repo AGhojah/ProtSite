9a4f6e529ff853c6ff0679c4d47cfadfceefc9f0
AGhojah <ghokah@gmail.com>
2019-05-20 23:37:20 +0300

    Adding Home Section

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,9 +3,15 @@
 // })
 let preloader = document.getElementById('preloader');
 
+// Time in seconds for fadeout effect
+let j = 2;
+
 window.onload = function(){
     setTimeout(()=>{
-        preloader.style.transition = "opacity 1s ease-out";
+        preloader.style.transition = `opacity ${j}s ease-out`;
         preloader.style.opacity = '0';
+        setTimeout(()=>{
+            preloader.style.display = 'none';
+        }, j*1000);
     }, 10);
 };
